20286188f6348cdd7561252b97288f63725b0890
Egorov <ola.ego@mail.ru>
2022-09-18 22:10:18 +0500

    Create to summator

---
+++ Summator.py
@@ -0,0 +1,8 @@
+#Summator
+
+number_1 = int(input(" Enter the first number: "))
+number_2 = int(input(" Enter the second number: "))
+
+number_3 = number_1 + number_2
+
+print(" Amount: ", number_3)
